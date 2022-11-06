40114dd3c9f6563c918951359800e0448105fa1e
steven9909 <sshyun@edu.uwaterloo.ca>
2022-11-05 20:42:33 -0400

    ga

---
+++ backend/server/simulation/ga/chromosome.py
@@ -0,0 +1,10 @@
+
+class Chromosome:
+    def __init__(self, force: float, angle: float):
+        """ 
+        Constructor for chromosome
+
+        Args:
+            force (float): force
+            angle (float): _description_
+        """
