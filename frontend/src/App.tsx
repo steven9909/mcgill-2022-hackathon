de47affc353e1a2c7d663922fe202ffe7600fb66
JStephenHuang <jstephhuang@gmail.com>
2022-11-06 02:55:14 -0500

    Textures for planets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three-orbitcontrols-ts";
 import "./App.css";
 
 const App = () => {
@@ -7,13 +8,17 @@ const App = () => {
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(
-      50,
+      75,
       window.innerWidth / window.innerHeight,
       1,
       1000
     );
 
-    camera.position.z = 96;
+    const canvasTexture = new THREE.TextureLoader().load(
+      "../public/textures/backgroudTexture.jpg"
+    );
+
+    scene.background = canvasTexture;
 
     const canvas = document.getElementById("threeJSCanvas");
     if (!canvas) return;
@@ -34,29 +39,57 @@ const App = () => {
     spotLight.position.set(128, 128, 64);
     scene.add(spotLight);
 
-    const radius = 15;
-    const heightSegments = 32;
-    const widthSegments = 16;
-    const geometrySphere = new THREE.SphereGeometry(
-      radius,
-      heightSegments,
-      widthSegments
-    );
-    const materialSphere = new THREE.MeshLambertMaterial();
-    const sphereMesh = new THREE.Mesh(geometrySphere, materialSphere);
-    scene.add(sphereMesh);
+    // Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-    const geometryBox = new THREE.BoxGeometry(20, 20, 20);
-    const materialBox = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(geometryBox, materialBox);
-    scene.add(boxMesh);
+    camera.position.set(0, 20, 100);
+    controls.update();
+
+    // Groups
+    const solarSystemGroup = new THREE.Group();
+    const earthOrbitGroup = new THREE.Group();
+    const moonOrbitGroup = new THREE.Group();
+
+    // Sun
+    const sunTexture = new THREE.TextureLoader().load(
+      "../public/textures/sunTexture.jpg"
+    );
+    const sunGeometry = new THREE.SphereGeometry(16);
+    const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+    const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+    solarSystemGroup.add(sunMesh);
+    scene.add(solarSystemGroup);
+    // Earth
+
+    const earthTexture = new THREE.TextureLoader().load(
+      "../public/textures/earthTexture.jpg"
+    );
+    const earthGeometry = new THREE.SphereGeometry(4);
+    const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
+    const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+    earthMesh.position.x = 50;
+    earthOrbitGroup.add(earthMesh);
+    scene.add(earthOrbitGroup);
+
+    // Moon
+
+    const moonTexture = new THREE.TextureLoader().load(
+      "../public/textures/moonTexture.jpg"
+    );
+    const moonGeometry = new THREE.SphereGeometry(2);
+    const moonMaterial = new THREE.MeshBasicMaterial({ map: moonTexture });
+    const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+    moonOrbitGroup.add(moonMesh);
+    moonOrbitGroup.position.x = 50;
+    moonMesh.position.x = 10;
+    earthOrbitGroup.add(moonOrbitGroup);
 
     const animate = () => {
-      sphereMesh.rotation.x += 0.01;
-      sphereMesh.rotation.y += 0.01;
+      sunMesh.rotation.x += 0.01;
+      earthOrbitGroup.rotation.y += 0.005;
+      moonOrbitGroup.rotation.y += 0.05;
 
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
+      controls.update();
 
       renderer.render(scene, camera);
       window.requestAnimationFrame(animate);
