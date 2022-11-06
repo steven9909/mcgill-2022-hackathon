eae19d53907a9b15a1ce11f6040aa6d5fa201a0a
jbennyhuang <j.benny.huang.developer@gmail.com>
2022-11-05 20:40:45 -0400

    change run sim script

--- backend/run_sim.sh
+++ backend/run_sim.sh
@@ -1,3 +1,3 @@
 SIM_SERVER=./sim_server/src/
 python -m grpc_tools.protoc -I./protos --python_out=$SIM_SERVER --pyi_out=$SIM_SERVER --grpc_python_out=$SIM_SERVER ./protos/simulation.proto
-python -m sim_server.src.main
+cd $SIM_SERVER && python main.py 
