c2227cc1bc5e840a98343b0e3bf6bf0f43880ab4
jbennyhuang <j.benny.huang.developer@gmail.com>
2022-11-06 13:37:04 -0500

    ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,72 +1,131 @@
-import { useEffect } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import "./App.css";
+import { socket } from "./services/websockets";
+import { BodyState } from "./types/body";
+import { Simulation } from "./types/simulation";
+
+const PLAYER_ID = -1;
+const SCALING_FACTOR = 0.0000000002;
+
+const textureMap: { [key: number]: string } = {
+  42: "/sunTexture.jpeg",
+  43: "/earthTexture.jpeg",
+  44: "/marsTexture.jpeg",
+  45: "/mercuryTexture.jpeg",
+  52: "/venusTexture.jpeg",
+  49: "/jupiterTexture.jpeg",
+  50: "/saturnTexture.jpeg",
+};
+
+const sizeMap: { [key: number]: number } = {
+  42: 5,
+  43: 1,
+  44: 2,
+  45: 2,
+  49: 4,
+  50: 3,
+};
 
 const App = () => {
+  const [simulation, setSimulation] = useState<Simulation | null>(null);
+  const [bodyStates, setBodyStates] = useState<BodyState[]>([]);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [renderer, setRenderer] = useState<THREE.WebGL1Renderer | null>(null);
+  const [scene, setScene] = useState<THREE.Scene | null>(null);
+  const [camera, setCamera] = useState<THREE.Camera | null>(null);
+  const [spheres, setSpheres] = useState<Record<number, THREE.Mesh>>({});
+
   useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const [width, height] = [window.innerWidth, window.innerHeight];
+
+    // setup render, scene, and camera
+    const renderer = new THREE.WebGL1Renderer({ canvas: canvasRef.current });
     const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
 
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
+    setRenderer(renderer);
+    setScene(scene);
+    setCamera(camera);
 
-    camera.position.z = 96;
+    // setup light
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.set(0, 0, 96);
 
-    const canvas = document.getElementById("threeJSCanvas");
-    if (!canvas) return;
-    const renderer = new THREE.WebGL1Renderer({
-      canvas,
-      antialias: true,
-    });
+    // configure renderer
+    renderer.setSize(width, height);
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    // configure scene
+    scene.add(pointLight);
+    // scene.background = new THREE.TextureLoader().load("/backgroudTexture.jpeg");
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
+    // configure camera
+    camera.position.set(0, 0, 768);
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(128, 128, 64);
-    scene.add(spotLight);
+    renderer.render(scene, camera);
 
-    const radius = 15;
-    const heightSegments = 32;
-    const widthSegments = 16;
-    const geometrySphere = new THREE.SphereGeometry(
-      radius,
-      heightSegments,
-      widthSegments
-    );
-    const materialSphere = new THREE.MeshLambertMaterial();
-    const sphereMesh = new THREE.Mesh(geometrySphere, materialSphere);
-    scene.add(sphereMesh);
+    return () => renderer.dispose();
+  }, [canvasRef]);
 
-    const geometryBox = new THREE.BoxGeometry(20, 20, 20);
-    const materialBox = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(geometryBox, materialBox);
-    scene.add(boxMesh);
-
-    const animate = () => {
-      sphereMesh.rotation.x += 0.01;
-      sphereMesh.rotation.y += 0.01;
-
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
+  useEffect(() => {
+    const callback = (ev: MessageEvent<string>) => {
+      setBodyStates(JSON.parse(ev.data));
     };
 
-    animate();
+    socket.addEventListener("message", callback);
+
+    return () => {
+      socket.removeEventListener("message", callback);
+    };
   }, []);
+
+  useEffect(() => {
+    if (!renderer || !scene || !camera || !canvasRef.current) return;
+
+    console.log(spheres);
+
+    if (Object.keys(spheres).length == 0) {
+      for (const bodyState of bodyStates) {
+        const geometry = new THREE.SphereGeometry(4, 32, 16);
+        const material = new THREE.MeshLambertMaterial();
+        // const material =
+        //   bodyState.bodyId < 0
+        //     ? new THREE.MeshLambertMaterial()
+        //     : new THREE.MeshBasicMaterial({
+        //         map: new THREE.TextureLoader().load(
+        //           textureMap[bodyState.bodyId]
+        //         ),
+        //       });
+
+        const sphere = new THREE.Mesh(geometry, material);
+        scene.add(sphere);
+        spheres[bodyState.bodyId] = sphere;
+      }
+
+      setSpheres(spheres);
+    }
+
+    for (const bodyState of bodyStates) {
+      spheres[bodyState.bodyId].position.set(
+        bodyState.positionX * SCALING_FACTOR,
+        bodyState.positionY * SCALING_FACTOR,
+        0
+      );
+    }
+
+    renderer.render(scene, camera);
+
+    return () => renderer.dispose();
+  }, [bodyStates]);
+
   return (
     <div>
-      <canvas id="threeJSCanvas" />
+      <canvas
+        ref={canvasRef}
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
     </div>
   );
 };
