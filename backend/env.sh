55f210e11c9cef6373cc18b724ec898706143ad7
jbennyhuang <j.benny.huang.developer@gmail.com>
2022-11-08 14:59:12 -0500

    updating env.sh

--- backend/env.sh
+++ backend/env.sh
@@ -2,4 +2,5 @@ export SUPABASE_URL=https://iaofxqrtpomnvesnevzb.supabase.co
 export SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhb2Z4cXJ0cG9tbnZlc25ldnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc2NjkyOTQsImV4cCI6MTk4MzI0NTI5NH0.T0N6-kcKcEzx0ZEE5A5cMXopp35arR5ss_wJSCOUYFE
 
 export REDIS_URL=localhost:6379
+export API_SERVER_URL=localhost:8080
 export SIM_SERVER_URL=localhost:50051
